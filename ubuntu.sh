05097d73747efc5cb61eded1d215f2ed7ecbeecd
Carlos Juan Gómez Peñalver <carlosjuangp@gmail.com>
2018-03-24 09:40:15 +0000

    CG feat(wrapper) Script to run scripts
    Make installation really easy

---
+++ ubuntu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -s https://raw.githubusercontent.com/carlosjgp/ubuntu-install-scripts/master/apt.sh | sh
+
+curl -s https://raw.githubusercontent.com/carlosjgp/ubuntu-install-scripts/master/curl.sh | sh
+
+curl -s https://raw.githubusercontent.com/carlosjgp/ubuntu-install-scripts/master/apt.sh | sh
