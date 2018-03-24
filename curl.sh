1d6f6f332a089fd80156d3087ff491ea28bf5bcd
Carlos Juan Gómez Peñalver <carlosjuangp@gmail.com>
2018-03-24 09:33:45 +0000

    CG feat(git) Pull git congif
    Get .gitconfig file from the repo

--- curl.sh
+++ curl.sh
@@ -5,7 +5,7 @@ cd ~
 
 echo Configure git
 curl -s https://gist.githubusercontent.com/adeekshith/cd4c95a064977cdc6c50/raw/bb54233668f5c56c1a19f0ce8faf3a89eff8c678/.git-commit-template.txt > ~/.gitmessage
-
+curl -s https://raw.githubusercontent.com/carlosjgp/ubuntu-install-scripts/master/git/.gitconfig > ~/.gitconfig
 
 echo Install Oh! My zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
