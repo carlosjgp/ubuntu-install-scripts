e552df6fc250986ce6ba7fb1e83d6f1582e8de2a
Carlos Juan Gómez Peñalver <carlosjuangp@gmail.com>
2018-06-09 00:49:48 +0100

    CG - Install especific K8s version and Helm

--- kubernetes.sh
+++ kubernetes.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+K8S_VERSION=v1.9.6
+HELM_VERSION=v2.8.2
+
 echo Install kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$K8S_VERSION/bin/linux/amd64/kubectl"
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
@@ -11,3 +14,10 @@ curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/miniku
 echo Kubectl aliases
 mkdir -p ~/aliases
 curl -s https://raw.githubusercontent.com/ahmetb/kubectl-alias/master/.kubectl_aliases > ~/aliases/kubectl_aliases
+
+echo Create Minikube with K8s 1.9.6
+minikube start --memory 8192 --cpus 2 --kubernetes-version "$K8S_VERSION"
+
+echo Install Helm
+export DESIRED_VERSION="$HELM_VERSION"
+curl -s https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
